1173583eab71b5d0044374b14e37bd69213afdb2
Expert <bekzodmuxtorxonov@gmail.com>
2023-01-28 08:01:21 +0500

    last2 commit

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -8,7 +8,8 @@ import android.view.View
 import android.widget.Button
 import android.widget.TextView
 
-class MainActivity : AppCompatActivity(),View.OnClickListener {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var zero: Button
     private lateinit var one: Button
     private lateinit var two: Button
     private lateinit var three: Button
@@ -18,25 +19,27 @@ class MainActivity : AppCompatActivity(),View.OnClickListener {
     private lateinit var seven: Button
     private lateinit var eight: Button
     private lateinit var nine: Button
-    private lateinit var zero: Button
-
+    private lateinit var clear: Button
+    private lateinit var plus_minus: Button
+    private lateinit var point: Button
+    private lateinit var foiz: Button
+    private lateinit var equal: Button
     private lateinit var plus: Button
     private lateinit var minus: Button
+    private lateinit var div: Button
+    private lateinit var multiply: Button
 
-    private lateinit var clear: Button
-    private lateinit var point: Button
-
-
+    private lateinit var answer: TextView
     private lateinit var operand: TextView
+    private var isok = false
+    private var symbol = false
+    private var number = false
 
-    private var ispoint = true
-    private var isdigit = false
-
-    @SuppressLint("SetTextI18n")
+    @SuppressLint("MissingInflatedId", "SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        zero = findViewById(R.id.zero)
         one = findViewById(R.id.one)
         two = findViewById(R.id.two)
         three = findViewById(R.id.three)
@@ -46,16 +49,13 @@ class MainActivity : AppCompatActivity(),View.OnClickListener {
         seven = findViewById(R.id.seven)
         eight = findViewById(R.id.eight)
         nine = findViewById(R.id.nine)
-        zero = findViewById(R.id.zero)
-
+        operand = findViewById(R.id.operand)
+        point = findViewById(R.id.point)
+        clear = findViewById(R.id.clear)
         plus = findViewById(R.id.plus)
         minus = findViewById(R.id.minus)
-
-        clear = findViewById(R.id.clear)
-        point = findViewById(R.id.point)
-
-        operand = findViewById(R.id.operand)
-
+        div = findViewById(R.id.div)
+        multiply = findViewById(R.id.multiply)
         one.setOnClickListener(this)
         two.setOnClickListener(this)
         three.setOnClickListener(this)
@@ -65,49 +65,57 @@ class MainActivity : AppCompatActivity(),View.OnClickListener {
         seven.setOnClickListener(this)
         eight.setOnClickListener(this)
         nine.setOnClickListener(this)
-        zero.setOnClickListener(this)
 
+
+        point.setOnClickListener {
+            if (!isok && !symbol) {
+                operand.text = operand.text.toString() + "."
+                isok = true
+            }
+        }
+        clear.setOnClickListener {
+            operand.text = "0"
+            isok = false
+            symbol = false
+        }
+        plus.setOnClickListener {
+            symbol_onClick(plus.text.toString())
+        }
+        minus.setOnClickListener {
+            symbol_onClick(minus.text.toString())
+        }
+        div.setOnClickListener {
+            symbol_onClick(div.text.toString())
+        }
+        multiply.setOnClickListener {
+            symbol_onClick(multiply.text.toString())
+        }
+    }
+
+    @SuppressLint("SetTextI18n")
+    override fun onClick(view: View) {
+        var btn = findViewById<Button>(view.id)
+        if (operand.text != ("0")) {
+            operand.text = operand.text.toString() + btn.text
+        } else operand.text = btn.text
+        symbol = true
+        number = true
 
     }
 
     @SuppressLint("SetTextI18n")
-    override fun onClick(view: View?) {
-
-    }
-
-    @SuppressLint("SetTextI18n")
-    fun addSimvol(simvol: String) {
-        if (isdigit) {
-            operand.text = operand.text.toString() + simvol
-            isdigit = false
-
+    fun symbol_onClick(simbol: String) {
+        if (symbol) {
+            operand.text = operand.text.toString() + simbol
+            symbol = false
+            number = false
+            if (number) {
+                isok = false
+            }
         } else {
-            if (operand.text != "0") {
-                operand.text = operand.text.dropLast(1)
-                    .toString() + simvol
-            }
+            operand.text = operand.text.dropLast(1)
+            operand.text = operand.text.toString() + simbol
         }
-        ispoint = true
     }
 
-    fun arr(s: String): MutableList<Any> {
-        val list = mutableListOf<Any>()
-        var temp = ""
-        for (i in s) {
-            if (i.isDigit() || i.equals(".")) {
-                temp += i
-            }
-            else{
-                list.add(temp)
-                list.add(i)
-                temp = ""
-            }
-        }
-        if (temp.isNotEmpty()){
-            list.add(temp)
-        }
-        Log.d("list", list.toString())
-
-        return list
-    }
 }
